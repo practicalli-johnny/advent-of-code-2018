c30c5e3971b009b6c00aac9d70a4bf74dc45de76
John Stevenson <john@jr0cket.co.uk>
2018-12-09 16:38:45 +0000

    New Leiningen project for Advent of Code 2018
    A new Leiningen project created with the default template.
    
    lein new advent-of-code-2018
    
    Updated licence to Creative Commons.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject advent-of-code-2018 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/jr0cket/advent-of-code-2018"
+  :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
+            :url "https://creativecommons.org"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ src/advent_of_code_2018/day_one.clj
@@ -0,0 +1,1 @@
+(ns advent-of-code-2018.day-one)
